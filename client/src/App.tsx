3ef1d487517e58c2240849891122c7f5d10298c2
Matěj <mckillem@tuta.io>
2023-11-12 18:52:15 +0100

    aktualizace

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.scss';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Header} from "./pages/Header";
+import {Footer} from "./pages/Footer";
+import {Main} from "./pages/Main";
 
 function App() {
-  return (
-    <div className="App">
-     <h1>sandbox</h1>
-    </div>
-  );
+
+    return (
+		<div className="App">
+			<Header></Header>
+			<BrowserRouter>
+				<Routes>
+					<Route path={"/"} element={<Main />}></Route>
+				</Routes>
+			</BrowserRouter>
+			<Footer></Footer>
+		</div>
+    );
 }
 
 export default App;
---
+++ client/src/pages/Main.tsx
@@ -0,0 +1,10 @@
+import "./Main.scss";
+
+export const Main = () => {
+
+	return (
+		<div id={"main"}>
+			<h1>obsah</h1>
+		</div>
+	);
+};
